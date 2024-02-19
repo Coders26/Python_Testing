6a9502fa4041df726ca2d98b167fde149aa7bd45
Coder26 <amateyaphoebe@gmail.com>
2024-02-19 22:25:45 +0000

    index

--- Test/test_index.py
+++ Test/test_index.py
@@ -13,11 +13,8 @@ def client():
 def test_index(client):
     competitions = loadCompetitions(datetime.strptime("2024-02-12 09:00:00", "%Y-%m-%d %H:%M:%S"))
     clubs = loadClubs()
-    # Envoyer une demande GET à la route index
     response = client.get('/')
-    # Vérification que le code de statut de la réponse est 200 (OK)
     assert response.status_code == 200
-    # Vérification que la réponse contient les noms des compétitions et des clubs
     for competition in competitions:
         assert competition['name'] in response.data.decode('utf-8')
         assert str(competition['numberOfPlaces']) in response.data.decode('utf-8')
